620b2b6c781834a235fcaf014bb205905f6dca29
MD kamrul Hassan <kamrul1157024@gmail.com>
2023-10-19 16:01:53 +0600

    add git hooks for synching

---
+++ init.sh
@@ -0,0 +1,4 @@
+chmod u+x ./sync-config.sh
+./sync-config.sh
+git config --local core.hooksPath .githooks/
+
---
+++ sync-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ----------sync tmux--------
+cp ./tmux.conf ~/tmux.conf
+# ----------------------------
+
+#-----------sync lazyvim------
+cp -r ./LazyVim ~/.config/nvim
+#------------------------------
+
