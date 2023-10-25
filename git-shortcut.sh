c73c98dd1be43a6f73328d09114a16a93906fe50
MD kamrul Hassan <kamrul1157024@gmail.com>
2023-10-25 15:06:48 +0600

    add gh review command

--- git-shortcut.sh
+++ git-shortcut.sh
@@ -3,6 +3,9 @@ alias gcommit="git commit -m $1"
 alias gpush="git push kamrul HEAD"
 alias gpull="git pull $1 master"
 
-alias review="gh pr checkout -f $1"
-
+function gh-review(){
+  repo_name=$(echo "$1"|cut -d "/" -f 5)
+  cd ~/workspace/$repo_name
+  gh pr checkout -f $1
+}
 
