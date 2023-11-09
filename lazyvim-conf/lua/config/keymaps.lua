d7f85f2ebe709944792b7ffc59529d5e564bcc0b
kamrul1157024 <kamrul1157024@gmail.com>
2023-11-09 11:12:55 +0600

    wqadd neovim shortcut to copy relative path

--- lazyvim-conf/lua/config/keymaps.lua
+++ lazyvim-conf/lua/config/keymaps.lua
@@ -1,5 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set('n', '<leader>yp', 'let @+ = expand("%")')
-vim.keymap.set('v', '<leader>cs', "'<, '>sort")
+
+vim.api.nvim_create_user_command("Cppath", function()
+  local path = vim.fn.expand("%")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
+
+vim.keymap.set("n", "<leader>yr", vim.cmd.Cppath, { desc="Copy relative path of current file", noremap=false})
+vim.keymap.set("v", "<leader>cs", "'<, '>sort")
