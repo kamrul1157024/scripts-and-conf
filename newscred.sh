dd3f19730847d731ad90c8bf4d7b77cede1e3182
kamrul1157024 <kamrul1157024@gmail.com>
2023-11-10 12:39:22 +0600

    auto open nvim editor

--- newscred.sh
+++ newscred.sh
@@ -17,3 +17,28 @@ e(){
   eval $command
 }
 
+open(){
+  export IFS="|"
+  window=0
+  for repoName in $1; do
+    if [$window -eq 0]; then
+      tmux rename-window -t $session:$window $repoName
+    else
+      tmux new-window -t $session:$window -n $repoName
+    fi
+
+    tmux send-keys -t $session:$window "cd ~/workspace/$repoName" C-m
+
+    window=$((window+1))
+  done
+}
+
+nc-editor(){
+  export session="editor"
+  tmux kill-session $session
+  tmux new-session -d -s $session
+  repoList="cmp-client|cmp-server|content-repo/content-repo|assets"
+  open "$repoList"
+  tmux attach-session -t $session
+}
+
--- source.sh
+++ source.sh
@@ -1,8 +1,8 @@
-source ~/scripts-and-conf/git-shortcut.sh
-source ~/scripts-and-conf/newscred.sh
-
 nvim(){
   tmux rename-window "$(basename $(pwd))"
   command nvim $1
 }
+
+source ~/scripts-and-conf/git-shortcut.sh
+source ~/scripts-and-conf/newscred.sh
 
