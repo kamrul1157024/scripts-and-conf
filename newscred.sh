7332c88e82ab80414d55ded40d1b5935f25d8b4f
kamrul1157024 <kamrul1157024@gmail.com>
2023-11-15 09:44:26 +0600

    add nc-init and logs command

--- newscred.sh
+++ newscred.sh
@@ -10,10 +10,18 @@ shell(){
   nc-docker shell $1
 }
 
+logs(){
+  nc-docker tail $1
+}
+
 # EX: Run `e yarn lint` on repo it should be able to run this command inside the docker container
 e(){
   command="nc-docker exec $(getRepoNameFromPwd) \"$@\""
   echo $command
   eval $command
+}
+
+nc-init(){
+  nc-docker up cmp-client content-repo preview-url-cacher
 }
 
