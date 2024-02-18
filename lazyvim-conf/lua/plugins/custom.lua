73d8bc07b8a97def94528332c2e176973c2a5515
kamrul1157024 <kamrul1157024@gmail.com>
2024-02-19 05:35:32 +0600

    add neovim surround plugin

--- lazyvim-conf/lua/plugins/custom.lua
+++ lazyvim-conf/lua/plugins/custom.lua
@@ -22,6 +22,16 @@ return {
       opts.linters_by_ft["*"] = { "cspell" }
     end,
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
   -- {
   --   "nvimtools/none-ls.nvim",
   --   opts = function(_, opts)
