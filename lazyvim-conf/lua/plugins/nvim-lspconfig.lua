6bdc9696b377fe7304d2506742f6a1e03bdec343
kamrul1157024 <kamrul1157024@gmail.com>
2024-02-19 05:46:22 +0600

    modularize plugin management codes for neovim

---
+++ lazyvim-conf/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        ruff_lsp = {},
+      },
+      setup = {
+        ruff_lsp = function()
+          require("lazyvim.util").lsp.on_attach(function(client, _)
+            if client.name == "ruff_lsp" then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+}
