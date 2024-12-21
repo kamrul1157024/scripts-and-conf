96e4e39ab2887fb5045641617b6790305b665145
kamrul1157024 <kamrul1157024@gmail.com>
2024-12-22 00:24:21 +0600

    update tailwind tools config

--- lazyvim-conf/lua/plugins/tailwind-tools.lua
+++ lazyvim-conf/lua/plugins/tailwind-tools.lua
@@ -1,5 +1,11 @@
 return {
   "luckasRanarison/tailwind-tools.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  name = "tailwind-tools",
+  build = ":UpdateRemotePlugins",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-telescope/telescope.nvim", -- optional
+    "neovim/nvim-lspconfig", -- optional
+  },
   opts = {}, -- your configuration
 }
