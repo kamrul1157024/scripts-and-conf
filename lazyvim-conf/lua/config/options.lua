3cb470ad205e6e4df0dab5447235afd3823b8386
kamrul1157024 <kamrul1157024@gmail.com>
2024-12-22 10:30:05 +0600

    remove nvim-cmp

--- lazyvim-conf/lua/config/options.lua
+++ lazyvim-conf/lua/config/options.lua
@@ -5,3 +5,4 @@ vim.g.autoformat = false
 vim.opt.spelllang = 'en_us'
 vim.opt.spell = true
 vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { "ExtraGroup" })
+vim.g.snacks_animate = false
