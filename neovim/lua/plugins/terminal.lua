fac70e24b4d079b880bb0fba76c0ea23a4b81cbd
kamrul1157024 <kamrul1157024@gmail.com>
2024-12-31 19:39:25 +0600

    fix terminal configuration

--- neovim/lua/plugins/terminal.lua
+++ neovim/lua/plugins/terminal.lua
@@ -1,1 +1,14 @@
-return { "akinsho/toggleterm.nvim", version = "*", config = true }
+  return {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      open_mapping = [[<c-_>]],
+      shade_terminalk = true,
+      auto_scroll = false,
+      direction = "float",
+      on_open = function(term)
+        local winid = term.window
+        vim.wo[winid].signcolumn = "no"
+      end,
+    },
+  }
