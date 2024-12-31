6e836f0970a8e5c494e42c6bccaa6e432517503f
kamrul1157024 <kamrul1157024@gmail.com>
2024-12-31 19:43:50 +0600

    add additional keymaps and options

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -1,2 +1,6 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.g.autoformat = false
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
+vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { "ExtraGroup" })
