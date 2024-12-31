98eeeb0e71ccaff67066b966a7c0dafa3c21c99c
kamrul1157024 <kamrul1157024@gmail.com>
2024-12-31 18:10:31 +0600

    add copilot support to the neovim and remove borders from the nvim-cmp

--- neovim/lua/config/lazy.lua
+++ neovim/lua/config/lazy.lua
@@ -28,7 +28,8 @@ require("lazy").setup({
     { import = "plugins" },
     { import = "plugins.lsp"},
     { import = "plugins.ui"},
-    { import = "plugins.git"}
+    { import = "plugins.git"},
+    { import = "plugins.ai"}
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
--- neovim/lua/plugins/nvim-cmp.lua
+++ neovim/lua/plugins/nvim-cmp.lua
@@ -5,47 +5,42 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp"
+    "hrsh7th/nvim-cmp",
   },
   config = function()
     cmp = require("cmp")
     cmp.setup({
-      
+
       window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
       },
       mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
-      sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-      }, {
-        { name = 'buffer' },
-      })
+      sources = cmp.config.sources({ { name = "copilot" }, { name = "nvim_lsp" }, { name = "buffer" } }),
     })
 
-    cmp.setup.cmdline({ '/', '?' }, {
+    cmp.setup.cmdline({ "/", "?" }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
-        { name = 'buffer' }
-      }
+        { name = "buffer" },
+      },
     })
-
-    cmp.setup.cmdline(':', {
+    cmp.setup.cmdline(":", {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
-        { name = 'path' }
+        { name = "path" },
       }, {
-        { name = 'cmdline' }
+        { name = "cmdline" },
       }),
-      matching = { disallow_symbol_nonprefix_matching = false }
+      matching = { disallow_symbol_nonprefix_matching = false },
     })
 
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  end
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  end,
 }
