54c5b695cdebb5b1c2b9fdb65fb034e17fb4446d
kamrul1157024 <kamrul1157024@gmail.com>
2024-12-31 23:06:42 +0600

    add shortcut switching between buffers

--- neovim/lua/config/keymaps.lua
+++ neovim/lua/config/keymaps.lua
@@ -53,19 +53,21 @@ end, { desc = "CopilotChat - Prompt actions" })
 
 local function switch_case()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  local word = vim.fn.expand('<cword>')
-  local word_start = vim.fn.matchstrpos(vim.fn.getline('.'), '\\k*\\%' .. (col+1) .. 'c\\k*')[2]
+  local word = vim.fn.expand("<cword>")
+  local word_start = vim.fn.matchstrpos(vim.fn.getline("."), "\\k*\\%" .. (col + 1) .. "c\\k*")[2]
 
   -- Detect camelCase
-  if word:find('[a-z][A-Z]') then
+  if word:find("[a-z][A-Z]") then
     -- Convert camelCase to snake_case
-    local snake_case_word = word:gsub('([a-z])([A-Z])', '%1_%2'):lower()
-    vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, {snake_case_word})
+    local snake_case_word = word:gsub("([a-z])([A-Z])", "%1_%2"):lower()
+    vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, { snake_case_word })
   -- Detect snake_case
-  elseif word:find('_[a-z]') then
+  elseif word:find("_[a-z]") then
     -- Convert snake_case to camelCase
-    local camel_case_word = word:gsub('(_)([a-z])', function(_, l) return l:upper() end)
-    vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, {camel_case_word})
+    local camel_case_word = word:gsub("(_)([a-z])", function(_, l)
+      return l:upper()
+    end)
+    vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, { camel_case_word })
   else
     print("Not a snake_case or camelCase word")
   end
@@ -77,7 +79,13 @@ vim.api.nvim_create_user_command("Cppath", function()
   vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {})
 
-vim.keymap.set("n", "<leader>yr", vim.cmd.Cppath, { desc="Copy relative path of current file", noremap=false})
+vim.keymap.set("n", "<leader>yr", vim.cmd.Cppath, { desc = "Copy relative path of current file", noremap = false })
 vim.keymap.set("v", "<leader>cs", "'<, '>sort")
-vim.keymap.set('n', '<leader>ctc', switch_case, { desc="Toggle between snake_case and camelCase", noremap = true, silent = true})
+vim.keymap.set(
+  "n",
+  "<leader>ctc",
+  switch_case,
+  { desc = "Toggle between snake_case and camelCase", noremap = true, silent = true }
+)
 
+vim.keymap.set("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
