6519b0f299b81e0bc85dffb7faa5fca23fb7d8d7
kamrul1157024 <kamrul1157024@gmail.com>
2025-01-01 11:52:50 +0600

    remove sync script

--- sync-config.sh
+++ sync-config.sh
@@ -3,8 +3,3 @@
 # ----------sync tmux--------
 cp -f ./tmux-conf/tmux.conf ~/.tmux.conf
 # ----------------------------
-
-#-----------sync lazyvim------
-rm -rf ~/.config/nvim/
-rsync -avz ./neovim/* ~/.config/nvim
-#------------------------------
