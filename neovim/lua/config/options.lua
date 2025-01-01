f75834bdd8e3954e2cbba65ec77dea830341f9d6
kamrul1157024 <kamrul1157024@gmail.com>
2025-01-01 14:14:27 +0600

    make system clipboard as default yank and paster register

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -7,3 +7,4 @@ vim.opt.smartindent = true
 vim.opt.shiftwidth = 2
 vim.opt.autoindent = true
 vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, { "ExtraGroup" })
+vim.api.nvim_set_option("clipboard","unnamed") -- To connect clipboard to default yank and paste register
