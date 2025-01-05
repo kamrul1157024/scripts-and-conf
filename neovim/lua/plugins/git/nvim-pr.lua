b7a77dce8244e9501382462cba7e68e4af554390
kamrul1157024 <kamrul1157024@gmail.com>
2025-01-05 09:55:43 +0600

    update plugin configs

--- neovim/lua/plugins/git/nvim-pr.lua
+++ neovim/lua/plugins/git/nvim-pr.lua
@@ -1,9 +1,13 @@
 return {
   "kamrul1157024/nvim-pr",
+  dir = "~/nvim-pr",
   config = function()
-    require("pr").setup()
+    require("nvim-pr").setup()
+    vim.keymap.set("n", "<leader>gprv", ":PR open<CR>", { desc = "View PR in the editor" })
+    vim.keymap.set("n", "<leader>gprb", ":PR open_in_browser<CR>", { desc = "Open PR in the browser" })
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
   },
 }
--- neovim/lua/plugins/nvim-treesitter.lua
+++ neovim/lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,7 @@ return {
     require'nvim-treesitter.configs'.setup {
       ensure_installed = "all",
       sync_install = false,
-      auto_install = true,
+      auto_install = false,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
