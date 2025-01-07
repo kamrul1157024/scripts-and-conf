039fd3a8daf08c1d73ddc12da6708ae5b899dd00
kamrul1157024 <kamrul1157024@gmail.com>
2025-01-07 13:05:24 +0600

    include running linters

--- neovim/lua/plugins/nvim-lint.lua
+++ neovim/lua/plugins/nvim-lint.lua
@@ -10,16 +10,15 @@ return {
       javascriptreact = { "eslint-lsp" },
       typescriptreact = { "eslint-lsp" },
       svelte = { "eslint-lsp" },
-      python = { "ruff" },
-      ["*"] = { "cspell" },
+      python = { "flake8" },
     }
-
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
         lint.try_lint()
+	lint.try_lint("cspell")
       end,
     })
 
--- neovim/lua/plugins/ui/lualine.lua
+++ neovim/lua/plugins/ui/lualine.lua
@@ -1,67 +1,75 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    lazy=false,
-    config = function()
-      require('lualine').setup {
-        options = {
-          icons_enabled = true,
-          theme = 'auto',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-          ignore_focus = {},
-          always_divide_middle = true,
-          always_show_tabline = true,
-          globalstatus = false,
-          refresh = {
-            statusline = 100,
-            tabline = 100,
-            winbar = 100,
-          }
-        },
-        sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = { {
-        "filename",
-        file_status = true, -- Displays file status (readonly status, modified status)
-        newfile_status = false, -- Display new file status (new file means no write after created)
-        path = 1, -- 0: Just the filename
-        -- 1: Relative path
-        -- 2: Absolute path
-        -- 3: Absolute path, with tilde as the home directory
-        -- 4: Filename and parent dir, with tilde as the home directory
-
-        shorting_target = 0, -- Shortens path to leave 40 spaces in the window
-        -- for other components. (terrible name, any suggestions?)
-        symbols = {
-          modified = "[+]", -- Text to show when the file is modified.
-          readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
-          unnamed = "[No Name]", -- Text to show for unnamed buffers.
-          newfile = "[New]", -- Text to show for newly created file before first write
-        },
-      }},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
-          lualine_y = {},
-          lualine_z = {}
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-      }
-      require("lualine").setup()
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "mfussenegger/nvim-lint" },
+  config = function()
+    local lint_progress = function()
+      local linters = require("lint").get_running()
+      if #linters == 0 then
+        return "󰦕"
+      end
+      return "󱉶 " .. table.concat(linters, ", ")
     end
-  },
+    require("lualine").setup({
+      options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        always_show_tabline = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 100,
+          tabline = 100,
+          winbar = 100,
+        },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = {
+          {
+            "filename",
+            file_status = true, -- Displays file status (readonly status, modified status)
+            newfile_status = false, -- Display new file status (new file means no write after created)
+            path = 1, -- 0: Just the filename
+            -- 1: Relative path
+            -- 2: Absolute path
+            -- 3: Absolute path, with tilde as the home directory
+            -- 4: Filename and parent dir, with tilde as the home directory
+
+            shorting_target = 0, -- Shortens path to leave 40 spaces in the window
+            -- for other components. (terrible name, any suggestions?)
+            symbols = {
+              modified = "[+]", -- Text to show when the file is modified.
+              readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+              unnamed = "[No Name]", -- Text to show for unnamed buffers.
+              newfile = "[New]", -- Text to show for newly created file before first write
+            },
+          },
+        },
+        lualine_x = { lint_progress, "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {},
+    })
+    require("lualine").setup()
+  end,
 }
