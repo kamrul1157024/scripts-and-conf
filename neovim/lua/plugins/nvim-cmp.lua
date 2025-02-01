6df9f5595dc82411cc794884a4bde49f0ccd0f3e
kamrul1157024 <kamrul1157024@gmail.com>
2025-02-01 13:34:25 +0600

    use copilot provider

--- neovim/lua/plugins/nvim-cmp.lua
+++ neovim/lua/plugins/nvim-cmp.lua
@@ -69,10 +69,8 @@ return {
         ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
       sources = {
-        -- Copilot Source
+        { name = "nvim_lsp", group_index = 2},
         { name = "copilot", group_index = 2 },
-        -- Other Sources
-        { name = "nvim_lsp", group_index = 2 },
         { name = "path", group_index = 2 },
         { name = "luasnip", group_index = 2 },
       },
